a43b0ee98b7ffd287b45c03be3470069f6b8df1f
Oskar Wong <oskar.wong@me.com>
2025-03-12 21:43:19 +0800

    Initial commit

--- src/main/java/org/fxexchange/Main.java
+++ src/main/java/org/fxexchange/Main.java
@@ -1,7 +1,11 @@
 package org.fxexchange;
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        System.out.println(fizzbuzz.getarray(5));
     }
 }
